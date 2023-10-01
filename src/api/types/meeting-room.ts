5052179b0f6d1337348d5185a3944a9c5b85f95d
James <jamesezhang@tencent.com>
2023-10-01 16:14:30 +0800

    feat: 会议室管理模块前端页面

---
+++ src/api/types/meeting-room.ts
@@ -0,0 +1,16 @@
+
+export interface CreateMeetingRoom {
+	name: string;
+	capacity: number;
+	location: string;
+	equipment: string;
+	description: string;
+}
+
+export interface UpdateMeetingRoom {
+	name: string;
+	capacity: number;
+	location: string;
+	equipment: string;
+	description: string;
+}
--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,36 +1,89 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Menu as AntdMenu, MenuProps } from 'antd';
 import './menu.css';
-
+import { useCallback, useMemo } from "react";
+import router from "../../router";
+const enum MenuItemKeyMap {
+  MEETING_ROOM_MANAGE = 'meetingRoomManage',
+  BOOKING_MANAGE = 'bookingManage',
+  USER_MANAGE = 'userManage',
+  STATISTICS = 'statistics',
+};
+const enum MenuItemKeyToPathMap {
+  MEETING_ROOM_MANAGE = '/meeting_room_manage',
+  BOOKING_MANAGE = '/booking_manage',
+  USER_MANAGE = '/user_manage',
+  STATISTICS = '/statistics',
+};
+// const MenuItemKeyToPathMap = {
+//   [MenuItemKeyMap.BOOKING_MANAGE]: '/meeting_room_manage',
+//   [MenuItemKeyMap.MEETING_ROOM_MANAGE]: '/booking_manage',
+//   [MenuItemKeyMap.USER_MANAGE]: '/user_manage',
+//   [MenuItemKeyMap.STATISTICS]: '/statistics',
+// };
+const MenuItemPathToKeyMap = {
+  [MenuItemKeyToPathMap.MEETING_ROOM_MANAGE]: MenuItemKeyMap.MEETING_ROOM_MANAGE,
+  [MenuItemKeyToPathMap.BOOKING_MANAGE]: MenuItemKeyMap.BOOKING_MANAGE,
+  [MenuItemKeyToPathMap.USER_MANAGE]: MenuItemKeyMap.USER_MANAGE,
+  [MenuItemKeyToPathMap.STATISTICS]: MenuItemKeyMap.STATISTICS,
+};
 const items: MenuProps['items'] = [
-    {
-        key: '1',
-        label: "会议室管理"
-    },
-    {
-        key: '2',
-        label: "预定管理"
-    },
-    {
-        key: '3',
-        label: "用户管理"
-    },
-    {
-        key: '4',
-        label: "统计"
-    }
+  {
+    key: MenuItemKeyMap.MEETING_ROOM_MANAGE,
+    label: "会议室管理"
+  },
+  {
+    key: MenuItemKeyMap.BOOKING_MANAGE,
+    label: "预定管理"
+  },
+  {
+    key: MenuItemKeyMap.USER_MANAGE,
+    label: "用户管理"
+  },
+  {
+    key: MenuItemKeyMap.STATISTICS,
+    label: "统计"
+  }
 ];
+type ItemType = MenuProps['items'] extends Array<infer T> ? T : Record<string, any>;
 
+// MenuClickEventHandler
 export function Menu() {
-    return <div id="menu-container">
-        <div className="menu-area">
-            <AntdMenu
-                defaultSelectedKeys={['3']}
-                items={items}
-            />
-        </div>
-        <div className="content-area">
-            <Outlet></Outlet>
-        </div>
+  const handleMenuItemClick = useCallback((info: ItemType) => {
+    let path = '';
+    switch (info.key) {
+      case MenuItemKeyMap.MEETING_ROOM_MANAGE:
+        path = MenuItemKeyToPathMap.MEETING_ROOM_MANAGE;
+        break;
+      case MenuItemKeyMap.BOOKING_MANAGE:
+        path = MenuItemKeyToPathMap.BOOKING_MANAGE;
+        break;
+      case MenuItemKeyMap.USER_MANAGE:
+        path = MenuItemKeyToPathMap.USER_MANAGE;
+        break;
+      case MenuItemKeyMap.STATISTICS:
+        path = MenuItemKeyToPathMap.STATISTICS;
+        break;
+    }
+    router.navigate(path);
+  }, []);
+  const location = useLocation();
+  const defaultSelectedKeys = useMemo(() => {
+    const path = location.pathname as string;
+    const selectedKeys = (MenuItemPathToKeyMap as Record<string, MenuItemKeyMap>)[path] ? [(MenuItemPathToKeyMap as Record<string, MenuItemKeyMap>)[path]] : [MenuItemKeyToPathMap.MEETING_ROOM_MANAGE];
+    return selectedKeys;
+  }, [location]);
+
+  return <div id="menu-container">
+    <div className="menu-area">
+      <AntdMenu
+        defaultSelectedKeys={defaultSelectedKeys}
+        items={items}
+        onClick={handleMenuItemClick}
+      />
     </div>
+    <div className="content-area">
+      <Outlet></Outlet>
+    </div>
+  </div>
 }
